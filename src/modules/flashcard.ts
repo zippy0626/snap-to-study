259aaa2482f6e4ed092669b4c6e8e006dbe2b059
zippy0626 <zippy0626@gmail.com>
2025-08-20 15:48:09 -0400

    revamped flashcard and summary classes

--- src/modules/flashcard.ts
+++ src/modules/flashcard.ts
@@ -1,17 +1,62 @@
-import type { FlashCard } from './types.ts';
+import type { IFlashCard } from "./types.ts";
 
-export class FlashCardModel {
-  static create(title: string, question: string, answer: string, tags?: string[]): FlashCard { //create with interface FlashCard
-    return {
-      title, question, answer, tags, createdAt: new Date()
+export class FlashCard implements IFlashCard {
+  // these are type declarations for FlashCard class
+  // they just tell you what the data needs to be
+  title: string;
+  front: string;
+  back: string;
+  tags?: string[] | undefined;
+  reviewedAt?: Date | undefined;
+  reviewCount?: number | undefined;
+  createdAt: Date;
+
+  // this is used to init the object
+  constructor(title: string, front: string, back: string, tags?: string[]) {
+    this.title = title;
+    this.front = front;
+    this.back = back;
+    this.tags = tags ?? [];
+    this.createdAt = new Date();
+  }
+
+  // syntax - methodName(params): returnType
+  flip(): void {
+    [this.front, this.back] = [this.back, this.front];
+  }
+
+  changeTitle(title: string): void {
+    this.title = title;
+  }
+
+  changeFront(front: string): void {
+    this.front = front;
+  }
+
+  changeBack(back: string): void {
+    this.back = back;
+  }
+
+  addTags(...tags: string[]): void {
+    for (const tag of tags) {
+      this.tags?.push(tag);
     }
   }
 
-  static review(flashCard: FlashCard): FlashCard {
-    return {
-      ...flashCard, //copy all exising properties
-      reviewedAt: new Date(),
-      reviewCount: (flashCard.reviewCount || 0) + 1
+  removeTags(...tags: string[]): void {
+    for (const tag of tags) {
+      let i = this.tags?.indexOf(tag);
+      if (i !== undefined && i > -1) {
+        this.tags?.splice(i, 1);
+      }
     }
+  }
+
+  changeReviewDateToToday(): void {
+    this.reviewedAt = new Date();
+  }
+
+  incrementReviewCount(count: number) {
+    this.reviewCount = (this.reviewCount ?? 0) + count;
   }
 }
--- src/modules/summary.ts
+++ src/modules/summary.ts
@@ -1,9 +1,25 @@
-import type { Summary } from "./types.ts";
+import type { ISummary } from "./types.ts";
 
-export class SummaryCardModel {
-  static create(title: string, text: string, source?: string): Summary { //create with interface Summary
-      return {
-        title, text, source, createdAt: new Date()
-      }
-    }
+export class SummaryCard implements ISummary {
+  // these are type declarations for FlashCard class
+  // they just tell you what the data needs to be
+  title: string;
+  text: string;
+  source?: string | undefined;
+  createdAt: Date;
+
+  constructor(title: string, text: string, source?: string) {
+    this.title = title;
+    this.text = text;
+    this.source = source;
+    this.createdAt = new Date();
+  }
+
+  changeText(text: string): void {
+    this.text = text;
+  }
+
+  changeTitle(title: string): void {
+    this.title = title;
+  }
 }
