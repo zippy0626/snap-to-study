296a1b6d7272a09e482733724369a695b807ebde
zippy0626 <zippy0626@gmail.com>
2025-08-20 15:47:54 -0400

    added functions to Interfaces

--- src/modules/types.ts
+++ src/modules/types.ts
@@ -1,16 +1,31 @@
-export interface Summary {
-  title: string
-  text: string
-  source?: string
+// these are like blueprints you need to follow for Objects
+export interface ISummary {
+  title: string;
+  text: string;
+  source?: string;
   createdAt: Date;
+
+  // functions
+  changeTitle(title: string): void;
+  changeText(text: string): void;
 }
 
-export interface FlashCard {
+export interface IFlashCard {
   title: string;
-  question: string;
-  answer: string;
-  tags?: string[]; //string arr
-  createdAt: Date;
-  reviewedAt?: Date; //optional args with ?
+  front: string;
+  back: string;
+  tags?: string[];
+  reviewedAt?: Date;
   reviewCount?: number;
+  createdAt: Date;
+
+  // functions
+  flip(): void;
+  changeTitle(title: string): void;
+  changeFront(front: string): void;
+  changeBack(back: string): void;
+  addTags(...tags: string[]): void;
+  removeTags(...tags: string[]): void;
+  changeReviewDateToToday(): void;
+  incrementReviewCount(count: number): void;
 }
